846f8d332f9e2e36c5468dd7d70de5071870f216
Ivan Glebov <glebov.vanya8@gmail.com>
2020-10-24 11:20:33 +0300

    vigenere.py_fixes

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -11,27 +11,28 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     ciphertext = ""
     key = ""
-    
-    while len(key) < len(plaintext): 
-        key += keyword
-    a = 0
-    for i in plaintext:
-        if i.isalpha():
-            lcFlag = i.islower()
-            if lcFlag:
-                if (ord(i) + ord(key[a]) - ord("a")) <= ord("z"):
-                    ciphertext += chr(ord(i) + ord(key[a]) - ord("a"))
+    if len(keyword) > 0:
+        while len(key) < len(plaintext): 
+            key += keyword
+        a = 0
+        for i in plaintext:
+            if i.isalpha():
+                lcFlag = i.islower()
+                if lcFlag:
+                    if (ord(i) + ord(key[a]) - ord("a")) <= ord("z"):
+                        ciphertext += chr(ord(i) + ord(key[a]) - ord("a"))
+                    else:
+                        ciphertext += chr(ord(i) + ord(key[a]) - ord("z") - 1)
                 else:
-                    ciphertext += chr(ord(i) + ord(key[a]) - ord("z") - 1)
+                    if (ord(i) + ord(key[a]) - ord("A")) <= ord("Z"):
+                        ciphertext += chr(ord(i) + ord(key[a]) - ord("A"))
+                    else:
+                        ciphertext += chr(ord(i) + ord(key[a]) - ord("Z") - 1)
             else:
-                if (ord(i) + ord(key[a]) - ord("A")) <= ord("Z"):
-                    ciphertext += chr(ord(i) + ord(key[a]) - ord("A"))
-                else:
-                    ciphertext += chr(ord(i) + ord(key[a]) - ord("Z") - 1)
-        else:
-            ciphertext += i
-        a += 1
-    # PUT YOUR CODE HERE
+                ciphertext += i
+            a += 1
+    else:
+        ciphertext = plaintext
 
     return ciphertext
 
@@ -49,30 +50,34 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     """
     plaintext = ""
     key = ""
-    while len(key) < len(ciphertext):
-        key += keyword
-    a = 0
-    for i in ciphertext:
-        if i.isalpha():
-            lcFlag = i.islower()
-            if lcFlag:
-                if ord(i) - (ord(key[a]) - ord("a")) >= ord("a"): 
-                    plaintext += chr(ord(i) - ord(key[a]) + ord("a"))
+    if len(keyword) > 0:
+        while len(key) < len(ciphertext):
+            key += keyword
+        a = 0
+        for i in ciphertext:
+            if i.isalpha():
+                lcFlag = i.islower()
+                if lcFlag:
+                    if ord(i) - (ord(key[a]) - ord("a")) >= ord("a"): 
+                        plaintext += chr(ord(i) - ord(key[a]) + ord("a"))
+                    else:
+                        plaintext += chr(ord(i) - ord(key[a]) + ord("z") + 1)
                 else:
-                    plaintext += chr(ord(i) - ord(key[a]) + ord("z") + 1)
-            else:
-                # ord(i) - keyLen
-                # keylen = ord(key[a]) - ord("A")
-                if ord(i) - (ord(key[a]) - ord("A")) >= ord("A"): 
-                    plaintext += chr(ord(i) - ord(key[a]) + ord("A"))
-                else:
-                    plaintext += chr(ord(i) - ord(key[a]) + ord("Z") + 1)
+                    # ord(i) - keyLen
+                    # keylen = ord(key[a]) - ord("A")
+                    if ord(i) - (ord(key[a]) - ord("A")) >= ord("A"): 
+                        plaintext += chr(ord(i) - ord(key[a]) + ord("A"))
+                    else:
+                        plaintext += chr(ord(i) - ord(key[a]) + ord("Z") + 1)
 
-        else:
-            plaintext += i
-        a += 1
+            else:
+                plaintext += i
+            a += 1
+    else:
+        plaintext = ciphertext
+    
     return plaintext
 
 
 # print(encrypt_vigenere("ATTACKATDAWN", "LEMON"))
-# print(decrypt_vigenere("AAA", "BCB"))
+# print(decrypt_vigenere("txMmDERKZvPaKerwXnRHMrljyeGMA,gKkqAJnZId NPFpI,rkokzKwTpkbnZfmq", ""))
